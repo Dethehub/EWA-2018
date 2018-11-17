4560d68c30d098ec99c7ef6443fbbbc8e7855949
Dethehub <karstenhertenstein@mail.de>
2018-11-17 01:28:22 +0100

    Dateien von Notebook in lokales Rep.

---
+++ Desktop/EWA 2018/myScripts.js
@@ -0,0 +1,19 @@
+﻿function pizzaHinzufuegen(element){
+	var id = element.id;
+	var selectBox = document.getElementById("Warenkorb_Bestellung");
+	var option = document.createElement("option")
+	option.text = id;
+	selectBox.add(option);
+	
+	if(id == "Margherita"){
+		var price = document.getElementById("preis");
+		var preisFLoat = parseFloat(price);
+		preisFloat = preisFloat + 3.5;
+		price = preisFloat.toFixed(2);
+		document.getElementById("preis").innerHTML = price;
+	}else if(id == "Spinat"){
+	
+	}else if(id == "Hawaii"){
+	
+	}
+}
